f92326455b76001af30c088e3148ae655973d387
Andy7961 <1662341860@qq.com>
2023-12-10 15:36:06 +0800

    两种方法写1-100内的所有奇数

---
+++ 写1-100内的所有奇数/方法1 用if.c
@@ -0,0 +1,12 @@
+//#include <stdio.h>
+//int main()
+//{
+//    int i = 1;
+//    while (i < 101)
+//    {
+//        if (i % 2 == 1)
+//            printf("%d ", i);
+//        i++;
+//    }
+//    return 0;
+//}
